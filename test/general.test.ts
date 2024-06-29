3fc90940e122d1d67085c9014eb9e037025845f6
Julien Flusin <50296610+jul-fls@users.noreply.github.com>
2024-06-30 00:38:16 +0200

    sonar and tests fix

--- test/general.test.ts
+++ test/general.test.ts
@@ -1,22 +1,22 @@
 import request from "supertest";
 import app from "../src/app";
 
-const baseURL = "http://localhost:7000";
-
 describe("Product API Tests", () => {
   let createdProductId: number;
 
   const newProduct = {
     name: "Test Product",
     stock: 10,
-    price: "19.99",
-    description: "A test product description",
-    color: "red"
+    details: {
+      price: "19.99",
+      description: "A test product description",
+      color: "red",
+    },
   };
 
   it("should create a new product", async () => {
     const response = await request(app)
-      .post(`${baseURL}/api/products`)
+      .post(`/api/products`)
       .send(newProduct)
       .expect(200);
 
@@ -26,13 +26,14 @@ describe("Product API Tests", () => {
 
   it("should retrieve a product by id", async () => {
     const response = await request(app)
-      .get(`${baseURL}/api/products/${createdProductId}`)
+      .get(`/api/products/${createdProductId}`)
       .expect(200);
 
     expect(response.body).toHaveProperty("name", newProduct.name);
     expect(response.body).toHaveProperty("stock", newProduct.stock);
-    expect(response.body).toHaveProperty("price", newProduct.price);
-    expect(response.body).toHaveProperty("description", newProduct.description);
-    expect(response.body).toHaveProperty("color", newProduct.color);
+    expect(response.body).toHaveProperty("details");
+    expect(response.body.details).toHaveProperty("price", newProduct.details.price);
+    expect(response.body.details).toHaveProperty("description", newProduct.details.description);
+    expect(response.body.details).toHaveProperty("color", newProduct.details.color);
   });
 });
