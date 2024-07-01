fdd3ab01e6ac9db6c20f063883502f56cb78a493
Julien Flusin <50296610+jul-fls@users.noreply.github.com>
2024-07-01 23:15:07 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,6 @@
 // Import with `import * as Sentry from "@sentry/node"` if you are using ESM
 import * as Sentry from "@sentry/node";
-const { nodeProfilingIntegration } = require("@sentry/profiling-node");
-
-Sentry.init({
-    dsn: "https://d286a0c40c84bf034f2b73d6dc191582@o4507378463080448.ingest.de.sentry.io/4507487341117520",
-    integrations: [
-        nodeProfilingIntegration(),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-
-    // Set sampling rate for profiling - this is relative to tracesSampleRate
-    profilesSampleRate: 1.0,
-});
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import express from "express";
 import * as dotevnv from "dotenv";
 import cors from "cors";
@@ -26,6 +14,18 @@ if (!process.env.PORT) {
 }
 
 const PORT = parseInt(process.env.PORT as string, 10);
+
+Sentry.init({
+  dsn: "https://d286a0c40c84bf034f2b73d6dc191582@o4507378463080448.ingest.de.sentry.io/4507487341117520",
+  integrations: [
+      nodeProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
 
 const app = express();
 
