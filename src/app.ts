1907cfd4095e7b80b9662db5ccbb6bc1834e02e4
Julien Flusin <50296610+jul-fls@users.noreply.github.com>
2024-07-01 23:04:19 +0200

    fix pipeline

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,19 @@
-import './instrument.ts';
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+
+Sentry.init({
+    dsn: "https://d286a0c40c84bf034f2b73d6dc191582@o4507378463080448.ingest.de.sentry.io/4507487341117520",
+    integrations: [
+        nodeProfilingIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+});
+
 import * as Sentry from "@sentry/node";
 import express from "express";
 import * as dotevnv from "dotenv";
