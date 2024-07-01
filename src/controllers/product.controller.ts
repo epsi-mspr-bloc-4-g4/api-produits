1f15d46ebaa1656c96637b65b90f8dffe017b30a
Julien Flusin <50296610+jul-fls@users.noreply.github.com>
2024-07-01 16:51:21 +0200

    test

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
-import { produceMessage } from "../../kafka/producer";
+import { produceMessage } from "../kafka/producer";
 
 const prisma = new PrismaClient();
 
